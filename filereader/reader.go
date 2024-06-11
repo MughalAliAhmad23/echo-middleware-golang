c340a4cb5efc3f31438dedc428d773a243895099
mughalaliahmad23 <mughalaliahmad23@gmail.com>
2024-06-11 10:08:07 +0500

    taking no of goroutines from user in param with text file

--- filereader/reader.go
+++ filereader/reader.go
@@ -17,36 +17,27 @@ func SpaceCounter(s string, wg *sync.WaitGroup, c chan<- string, a []string) {
 	defer wg.Done()
 
 	var iwg sync.WaitGroup
-
 	totalspaces := 0
-
 	space := make([]int, len(a))
-	for j := 0; j < len(a); j++ {
 
+	for j := 0; j < len(a); j++ {
 		iwg.Add(1)
 
 		go func(s string, iwg *sync.WaitGroup, ind int) {
-			//	fmt.Println("index value", space[ind])
 			defer iwg.Done()
-			//	space[j] := 0
 
 			for i := 0; i < len(s); i++ {
 				if s[i] == ' ' {
-
 					space[ind]++
-
 				}
 			}
-			//fmt.Println("index after addition:", space[ind])
-			//totalspaces = +space[ind]
 		}(a[j], &iwg, j)
 	}
 	iwg.Wait()
-	//totalspaces := 0
+
 	for _, count := range space {
 		totalspaces += count
 	}
-	//fmt.Println("totalspaces:", totalspaces)
 
 	resp := fmt.Sprintf("%d totalspaces:", totalspaces)
 	fmt.Println(resp)
@@ -57,10 +48,7 @@ func VowelsCounter(s string, wg *sync.WaitGroup, c chan<- string, a []string) {
 	defer wg.Done()
 
 	totalvowels := 0
-
 	var iwg sync.WaitGroup
-	//var mu sync.Mutex
-
 	vowels := make([]int, len(a))
 
 	for i := 0; i < len(a); i++ {
@@ -68,60 +56,47 @@ func VowelsCounter(s string, wg *sync.WaitGroup, c chan<- string, a []string) {
 
 		go func(s string, iwg *sync.WaitGroup, ind int) {
 			defer iwg.Done()
-			//vowels := 0
+
 			for i := 0; i < len(s); i++ {
 				if s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' {
-
 					vowels[ind]++
-
 				}
 			}
-			// mu.Lock()
-			// totalvowels += vowels
-			// mu.Unlock()
 
 		}(a[i], &iwg, i)
 	}
 	iwg.Wait()
+
 	for _, count := range vowels {
 		totalvowels += count
 	}
+
 	resp := fmt.Sprintf("%d total vowels:", totalvowels)
 	fmt.Println(resp)
 	c <- resp
 }
 
-// func LineCounter(s string, wg *sync.WaitGroup, c chan<- string) {
-// 	defer wg.Done()
-// 	lines := 0
-// 	for i := 0; i < len(s); i++ {
-// 		if s[i] == '.' {
-// 			lines++
-// 		}
-// 	}
-// 	resp := fmt.Sprintf("%d total lines :", lines)
-// 	fmt.Println(resp)
-// 	c <- resp
-// }
-
 func Wordfrequeny(s string, wg *sync.WaitGroup, c chan<- string, a []string) {
 	defer wg.Done()
 
 	var iwg sync.WaitGroup
-
-	// totalwordcount := make(map[string]int, len(a))
-
 	wordscount := make([]map[string]int, len(a))
+	temp := make([]string, len(a))
+
 	for i := range wordscount {
 		wordscount[i] = make(map[string]int)
 	}
-	temp := make([]string, len(a))
+
 	for i := 0; i < len(a); i++ {
 		iwg.Add(1)
+
 		go func(s string, iwg *sync.WaitGroup, ind int) {
 			defer iwg.Done()
+
 			for j := 0; j < len(s); j++ {
+
 				if s[j] == ' ' || j == len(s)-1 {
+
 					if val, key := wordscount[ind][temp[ind]]; key {
 						wordscount[ind][temp[ind]] = val + 1
 					} else {
@@ -137,15 +112,9 @@ func Wordfrequeny(s string, wg *sync.WaitGroup, c chan<- string, a []string) {
 
 	}
 	iwg.Wait()
-	// for index, val := range wordscount {
-	// 	fmt.Println(index, val)
-	// }
 
-	// for k := 0; k < len(wordscount); k++ {
-
-	// }
-	//var mergemap map[string]int
 	mergemap := make(map[string]int)
+
 	for i := 0; i < len(wordscount); i++ {
 
 		for key := range wordscount[i] {
@@ -159,7 +128,6 @@ func Wordfrequeny(s string, wg *sync.WaitGroup, c chan<- string, a []string) {
 
 		}
 	}
-	//fmt.Println(mergemap)
 
 	resp := fmt.Sprintf("%v word frequencies:", mergemap)
 	fmt.Println(resp)
@@ -172,29 +140,24 @@ func Wordcounter(s string, wg *sync.WaitGroup, c chan<- string, a []string) {
 
 	totalwords := 0
 	var iwg sync.WaitGroup
-	//var mu sync.Mutex
 	words := make([]int, len(a))
-	//spaces := make([]int, len())
+
 	for i := 0; i < len(a); i++ {
 		iwg.Add(1)
+
 		go func(s string, iwg *sync.WaitGroup, ind int) {
 			defer iwg.Done()
-			//spaces := 0
-			//words := 0
+
 			for j := 0; j < len(s); j++ {
 				if s[j] == ' ' || j == len(s)-1 {
 					words[ind]++
 				}
 			}
 
-			// mu.Lock()
-			// words = spaces + 1
-			// totalwords += words
-			// mu.Unlock()
-
 		}(a[i], &iwg, i)
 	}
 	iwg.Wait()
+	
 	for _, count := range words {
 		totalwords += count
 	}
